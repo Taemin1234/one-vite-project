2cdd0d6ac0c18f6d7e2a85657e36742c94930aa6
King sejong <cestmoi7531@gmail.com>
2025-02-05 21:42:05 +0900

    react hooks

--- src/components/HookExam.jsx
+++ src/components/HookExam.jsx
@@ -2,18 +2,7 @@
 // 1. 함수 컴포넌트 , 커스텀 훅 내부에서만 호출 가능
 // 2. 조건부로 호출될 수 없다.(반복문, 조건문에서 사용불가)
 // 3. 나만의 훅(custom hook) 제작 가능
-
-import { useState } from "react";
-
-function useInput() {
-  const [input, setInput] = useState("");
-
-  const onChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  return [input, onChange];
-}
+import useInput from "./../hooks/useInput";
 
 const HookExam = () => {
   const [input, onChange] = useInput();
