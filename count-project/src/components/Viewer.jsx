f09fed05f7bb3ccf09266e4eba0357a275f5906b
King sejong <cestmoi7531@gmail.com>
2025-02-06 00:25:59 +0900

    프로젝트1

---
+++ count-project/src/components/Viewer.jsx
@@ -0,0 +1,10 @@
+const Viewer = () => {
+  return (
+    <>
+      <div>now count :</div>
+      <h1>0</h1>
+    </>
+  );
+};
+
+export default Viewer;
--- src/components/HookExam.jsx
+++ src/components/HookExam.jsx
@@ -2,7 +2,7 @@
 // 1. 함수 컴포넌트 , 커스텀 훅 내부에서만 호출 가능
 // 2. 조건부로 호출될 수 없다.(반복문, 조건문에서 사용불가)
 // 3. 나만의 훅(custom hook) 제작 가능
-import useInput from "./../hooks/useInput";
+import useInput from "../hooks/useInput";
 
 const HookExam = () => {
   const [input, onChange] = useInput();
