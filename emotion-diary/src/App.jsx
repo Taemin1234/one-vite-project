4adb6823c751f64dcd1d5c5fe1b55f7663bdd744
King sejong <cestmoi7531@gmail.com>
2025-02-23 21:51:44 +0900

    감정 다이어리 준비

---
+++ emotion-diary/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
