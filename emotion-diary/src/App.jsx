b5b275a515938ce46f8b939f09cd8d881d72bbd9
King sejong <cestmoi7531@gmail.com>
2025-02-24 22:32:27 +0900

    리소스 및 공통 컴포넌트 제작

--- emotion-diary/src/App.jsx
+++ emotion-diary/src/App.jsx
@@ -1,18 +1,38 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
+import { getEmotionImage } from "./util/get-emotion-images";
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button text={"1234"} onClick={onClickButton} />
+      <Button text={"1234"} type={"POSITIVE"} onClick={onClickButton} />
+      <Button text={"1234"} type={"NEGATIVE"} onClick={onClickButton} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
