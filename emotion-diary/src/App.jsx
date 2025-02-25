4a02d2302b3393fc9e81d611ee2fc9e4da767139
stm <kingsj1418@gmail.com>
2025-02-25 15:55:01 +0900

    edit 컴포넌트추가

--- emotion-diary/src/App.jsx
+++ emotion-diary/src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
 import Button from "./components/Button";
 import Header from "./components/Header";
+
 
 import { getEmotionImage } from "./util/get-emotion-images";
 
@@ -30,6 +32,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
